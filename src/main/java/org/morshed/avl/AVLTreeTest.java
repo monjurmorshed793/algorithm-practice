c7783148bd48ac1ddc0b2e14b4a29676afab20d1
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-06-23 11:22:14 +0600

    basic AVL tree formation added.

---
+++ src/main/java/org/morshed/avl/AVLTreeTest.java
@@ -0,0 +1,10 @@
+package org.morshed.avl;
+
+public class AVLTreeTest {
+  public static void main(String[] args) {
+    var tree = new AVLTree();
+    tree.insert(10);
+    tree.insert(20);
+    tree.insert(30);
+  }
+}
