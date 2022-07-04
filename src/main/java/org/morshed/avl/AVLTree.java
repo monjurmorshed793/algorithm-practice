a05eb5082648571d0961dec832d321db1a441ec2
monju <monjurmorshed793@gmail.com>
2022-07-04 15:35:18 +0600

    left-right rotation logic added.

--- src/main/java/org/morshed/avl/AVLTree.java
+++ src/main/java/org/morshed/avl/AVLTree.java
@@ -17,10 +17,10 @@ public class AVLTree {
     }
   }
 
-  private AVLNode root;
+  private AVLNode node;
 
   public void insert(int value){
-    root =  insert(root, value);
+    node =  insert(node, value);
   }
 
   private AVLNode insert(AVLNode root, int value){
@@ -33,23 +33,50 @@ public class AVLTree {
       root.rightChild = insert(root.rightChild, value);
 
 
-    root.height = Math.max(height(root.leftChild), height(root.rightChild))+1;
+    setHeight(root);
 
-    balance(root);
-    return root;
+    return balance(root);
   }
 
-  private void balance(AVLNode root){
+  private AVLNode balance(AVLNode root){
     if(isLeftHeavy(root)){
       if(balanceFactor(root.leftChild)<0)
-        System.out.println("Left Rotate "+ root.leftChild.value);
-      System.out.println("Right Rotate "+ root.value);
+        root.leftChild = rotateLeft(root.leftChild);
+      return rotateRight(root);
     }
     else if(isRightHeavy(root)){
       if(balanceFactor(root.rightChild)>0)
-        System.out.println("Right Rotate "+ root.rightChild.value);
-      System.out.println("Left Rotate "+ root.value);
+        root.rightChild =  rotateRight(root.rightChild);
+      return rotateLeft(root);
     }
+    return root;
+  }
+
+  private AVLNode rotateLeft(AVLNode root){
+    var newRoot = root.rightChild;
+    root.rightChild = newRoot.leftChild;
+    newRoot.leftChild = root;
+
+    setHeight(root);
+    setHeight(newRoot);
+
+    return newRoot;
+  }
+
+  private AVLNode rotateRight(AVLNode root){
+    var newRoot = root.leftChild;
+    root.leftChild = newRoot.rightChild;
+    newRoot.rightChild = root;
+
+    setHeight(root);
+    setHeight(newRoot);
+
+    return newRoot;
+  }
+
+  private void setHeight(AVLNode node){
+    this.node.height = Math.max(height(this.node.leftChild), height(this.node.rightChild))+1;
+
   }
 
   private int height(AVLNode node){
--- src/main/java/org/morshed/avl/AVLTreeTest.java
+++ src/main/java/org/morshed/avl/AVLTreeTest.java
@@ -4,7 +4,7 @@ public class AVLTreeTest {
   public static void main(String[] args) {
     var tree = new AVLTree();
     tree.insert(10);
-    tree.insert(30);
     tree.insert(20);
+    tree.insert(30);
   }
 }
