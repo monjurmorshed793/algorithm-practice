b9c94ff87b79114a28de8fd3b6781470fedf46a1
monju <monjurmorshed793@gmail.com>
2022-07-05 11:28:28 +0600

    Remove feature added in Heap

--- src/main/java/org/morshed/heap/Heap.java
+++ src/main/java/org/morshed/heap/Heap.java
@@ -11,6 +11,48 @@ public class Heap {
     bubbleUp();
   }
 
+  public void remove(){
+    if(isEmpty())
+      throw new IllegalStateException();
+    items[0] = items[--size];
+    var index = 0;
+    while(index<=size &&  isValidParent(index)){
+      var largetChildIndex = largerChildIndex(index);
+      swap(index, largetChildIndex);
+      index = largetChildIndex;
+    }
+  }
+
+  public boolean isEmpty(){
+    return size ==0;
+  }
+
+  private int largerChildIndex(int index){
+    return (leftChild(index) > rightChild(index))?
+      leftChildIndex(index):
+      rightChildIndex(index);
+  }
+
+  private boolean isValidParent(int index){
+    return items[index] >= leftChild(index) &&
+      items[index] >= rightChild(index);
+  }
+
+  private int leftChild(int index){
+    return leftChildIndex(index);
+  }
+
+  private int rightChild(int index){
+    return rightChildIndex(index);
+  }
+
+  private int leftChildIndex(int index){
+    return index * 2 + 1;
+  }
+
+  private int rightChildIndex(int index){
+    return index * 2 + 2;
+  }
   public boolean iszFull(){
     return size == items.length;
   }
