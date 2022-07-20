827b91321e36d0a64b85530d5a32e5b31acb3966
monju <monjurmorshed793@gmail.com>
2022-07-20 22:05:09 +0600

    Weighted graph addition of nodes and edges features added.

---
+++ src/main/java/org/morshed/weighted/graph/WeightedGraphTest.java
@@ -0,0 +1,13 @@
+package org.morshed.weighted.graph;
+
+public class WeightedGraphTest {
+  public static void main(String[] args) {
+    var graph = new WeightedGraph();
+    graph.addNode("A");
+    graph.addNode("B");
+    graph.addNode("C");
+    graph.addEdge("A", "B", 3);
+    graph.addEdge("A", "C", 2);
+    graph.print();
+  }
+}
